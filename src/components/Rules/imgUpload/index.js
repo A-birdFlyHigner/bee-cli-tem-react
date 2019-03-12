d89fe060a60d434f4b7cb78d595e4e298d605076
bozhou <liqiang@gegejia.com>
2019-03-12 16:30:15 +0800

    feat: imgUpload  参数放到options，增加宽高校验、types动态传入

--- src/components/Rules/imgUpload/index.js
+++ src/components/Rules/imgUpload/index.js
@@ -15,6 +15,9 @@ const DEFAULT_CONFIG = {
   // width: '',
   // height: '',
   // minWidth: '',
+  // maxWidth: '',
+  // minHeight: '',
+  // maxHeight: '',
 };
 
 function readFile(file) {
@@ -43,9 +46,9 @@ function imgWH(imgSrc) {
   });
 }
 
-export default (options = {}) => {
+export default (ruleConf = {}) => {
   return (leForm) => {
-    const name = options.name || DEFAULT_OPTIONS.name;
+    const name = ruleConf.name || DEFAULT_OPTIONS.name;
     const stateKey = `${DEFAULT_STATE_KEY}_${name}`;
 
     const postFile = (file, whSize) => {
@@ -79,7 +82,10 @@ export default (options = {}) => {
     };
 
     const regTypeSize = file => {
-      const isJPG = DEFAULT_CONFIG.types.indexOf(file.type) > -1;
+      const {options = {}} = ruleConf
+      const isJPG = options.types ? 
+      options.types.indexOf(file.type) > -1 : 
+      DEFAULT_CONFIG.types.indexOf(file.type) > -1;
       const size = options.size || DEFAULT_CONFIG.size;
       if (!isJPG) {
         message.warning('上传图片格式不正确');
@@ -97,8 +103,9 @@ export default (options = {}) => {
         if (!regTypeSize(file)) {
           return reject();
         }
-        const { width, height, minWidth } = options;
-        if (!width && !height && !minWidth) {
+        const { options = {} } = ruleConf
+        const { width, height, minWidth, maxWidth, minHeight, maxHeight } = options;
+        if (!width && !height && !minWidth && !maxWidth && !minHeight && !maxHeight) {
           return readFile(file).then(res => {
             return imgWH(res).then(rs => {
               postFile(file, {
@@ -121,6 +128,18 @@ export default (options = {}) => {
             }
             if (minWidth && rs.width < minWidth) {
               message.warning(`请上传图片最小宽度为${minWidth}px的图片`);
+              return reject();
+            }
+            if (maxWidth && rs.width > maxWidth) {
+              message.warning(`请上传图片最大宽度为${maxWidth}px的图片`);
+              return reject();
+            }
+            if (minHeight && rs.height < minHeight) {
+              message.warning(`请上传图片最小高度为${minHeight}px的图片`);
+              return reject();
+            }
+            if (maxHeight && rs.height > maxHeight) {
+              message.warning(`请上传图片最大高度为${maxHeight}px的图片`);
               return reject();
             }
             postFile(file, {
@@ -147,11 +166,13 @@ export default (options = {}) => {
     const children = isLoading => {
       let len = leForm.getValue(name);
       if (len === undefined) {
-        len = options.value ? options.value.length : 0;
+        len = ruleConf.value ? ruleConf.value.length : 0;
       } else {
         len = len ? len.length : 0;
       }
-      return len >= (options.limit || DEFAULT_CONFIG.limit) ? null : (
+      const {options = {}} = ruleConf
+      const {limit} = options
+      return len >= (limit || DEFAULT_CONFIG.limit) ? null : (
         <div>
           <Icon type={isLoading ? 'loading' : 'plus'} />
           <div className="ant-upload-text">Upload</div>
@@ -159,9 +180,9 @@ export default (options = {}) => {
       );
     };
 
-    const settings = _.merge({}, DEFAULT_OPTIONS, options, {
+    const settings = _.merge({}, DEFAULT_OPTIONS, ruleConf, {
       props: (props) => {
-        const preProps = options.props || (() => {});
+        const preProps = ruleConf.props || (() => {});
         const result = typeof preProps === 'function' ? preProps() : preProps;
         const curProps = {
           beforeUpload,
