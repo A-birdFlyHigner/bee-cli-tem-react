d4dfc5e1e504073e978e6a91935edbda008b0e80
太一 <taiyi@gegejia.com>
2019-08-08 11:09:03 +0800

    refactor: project clean

--- src/defaultSettings.js
+++
@@ -1,6 +0,0 @@
-const defaultSettings = require('../common/defaultSettings')
-
-module.exports = {
-  ...defaultSettings,
-  title: '小区乐后台'
-};
--- src/static/commonMessage.js
+++
@@ -1,8 +0,0 @@
-
-export default {
-  logisticsMethod: ['','落地配','入仓','快递配送'],
-  logisticsType: ['','次日达','预售'],
-  reviewStatus: ['待审核', '审核通过', '审核拒绝'],
-  allsaleStatus: ['全部','出售中','正在预热','等待上线'],
-  adminreviewStatus: ['','待审核','审核通过','审核拒绝']
-}
