785ba185f095e9b8b67e6340e6a2dbb2bc9e0541
太一 <taiyi@gegejia.com>
2019-08-23 11:25:04 +0800

    chore: add settings.json

--- src/views/nav1/routes.ts
+++ src/views/nav1/routes.ts
@@ -4,7 +4,7 @@
  * @Author: 太一
  * @Date: 2019-08-22 15:16:53
  * @LastEditors: 太一
- * @LastEditTime: 2019-08-23 11:15:20
+ * @LastEditTime: 2019-08-23 11:18:09
  */
 import { RoutesConfig } from 'router'
 
@@ -14,7 +14,7 @@ export const routes: RoutesConfig[] = [
     component: () => import('@views/nav1/page1')
   },
   {
-    path: '/nav1/page1',
+    path: '/nav1/page2',
     component: () => import('@views/nav1/page2')
   }
 ]
